8b0ff51dedefa30bfbf969b7d001776c501d1df7
Payad <swordoflight8847@yahoo.com>
2021-12-10 14:39:40 -0800

    update to html and css on media queries

--- assets/JS folder/app.js
+++ assets/JS folder/app.js
@@ -1,7 +1,7 @@
 // Menu Show
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId);
-    nav = document.getElementById(navId);
+    const nav = document.getElementById(navId);
 
     if (toggle && nav)  {
         toggle.addEventListener("click", () => {
